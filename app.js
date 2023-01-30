314e50dd4450138178c9a14f0246fb12e088f5b5
John Grudzinski <johngrudzinski@yahoo.com>
2023-01-29 20:50:19 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let adviceId;
+
+document.getElementById("generate-advice-button").addEventListener("click", function(){
+    //generate random advice id
+    adviceId = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+
+    //fetch advice from api
+    fetch("https://api.adviceslip.com/advice/" + adviceId)
+    .then(res => res.json())
+    .then(data => {
+        //display advice in element with id "advice-text"
+        document.getElementById("advice-text").innerHTML = data.slip.advice;
+        document.getElementById("advice-ID").innerHTML = "ADVICE #" + data.slip.id;
+    });
+});
+
+
+
+
+
+
+
+
